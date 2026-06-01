27fc7f5a7eb24a766c0c89c23f88cf244c1a1e1f
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-01 09:00:52 -0300

    criando as funcionalidades para cache
    criando as funcionalidades para cache

--- routes/rotasIndex.js
+++ routes/rotasIndex.js
@@ -5,4 +5,7 @@ const controllerIndex = require('../controller/controllerIndex.js');
 /* GET home page */
 router.get('/', controllerIndex.index);
 
+/* Página serviços */
+router.get('/servicos', controllerIndex.lista_servicos);
+
 module.exports = router;
